08665fd2531f381b46b7678d86153f844c3ab340
Madison Lutz <mlutz060@gmail.com>
2023-10-31 11:06:37 -0400

    First commit Angular todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from "./item"
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+
+  filter: "all" | "active" | "done" = "all";
+
+  allItems = [
+    { description:  "wake up", done: true },
+    { description:  "eat breakfast", done: true },
+    { description:  "go to work", done: true },
+    { description:  "eat lunch", done: false },
+    { description:  "go home", done: false }
+  ];
+
+  get items(){
+    if(this.filter === "all"){
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+    this.filter === "done" ? item.done : !item.done
+    );
+  }
+
+  addItem(description: string){
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+  remove(item: Item){
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
 }
